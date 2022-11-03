6b71b3143842b131ea87aeb72d0ee1120caf5917
Justin Liu <liujustin53@gmail.com>
2022-11-03 07:15:00 -0400

    getreq

--- src/GETRequest.tsx
+++ src/GETRequest.tsx
@@ -1,24 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function GETRequest () {
+function GETRequest() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState("");
 
-  const makeRequest = async () => {
-    try {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`);
-    } catch (err) {
-      setError(err.message);
-      setData(null);
-    }
-  }
-
-
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?_limit=10`
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        setData(await response.json());
+        setError("");
+      } catch (err) {
+        let message;
+        if (err instanceof Error) message = err.message;
+        else message = String(error);
+        setError(message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    makeRequest();
+  }, [error]);
 
   return (
-    <div>Our TSX</div>
+    <div className="App">
+      <h1>API Posts</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>{data && <h3>{data}</h3>}</ul>
+    </div>
   );
-};
+}
 
 export default GETRequest;
