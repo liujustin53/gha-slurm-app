7d6ee9943b269d9b6f5ac7dbf716721c14a82ed1
Justin Liu <liujustin53@gmail.com>
2022-11-03 04:25:45 -0400

    GETRequest

---
+++ src/GETRequest.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+function GETRequest () {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async makeRequest() {
+
+  };
+
+
+
+  return (
+    <div>Our TSX</div>
+  );
+};
+
+export default GETRequest;
