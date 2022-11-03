bbf1ee510d26fbc7f86ac55f7414a716b642a485
liujustin53 <liujustin53@gmail.com>
2022-11-03 08:36:18 +0000

    getrq

--- src/GETRequest.tsx
+++ src/GETRequest.tsx
@@ -1,13 +1,18 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 
 function GETRequest () {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  async makeRequest() {
-
-  };
+  const makeRequest = async () => {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`);
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    }
+  }
 
 
 
